97a95d9e87fb241f2328fe060cb4d6407750d162
nishant-s7 <97207366+nishant-s7@users.noreply.github.com>
2024-03-08 12:14:38 +0530

    animal apis

--- server/controller/animalController.js
+++ server/controller/animalController.js
@@ -1,4 +1,5 @@
 const Animal = require("../model/Animal");
+const Product = require("../model/Product");
 
 exports.addAnimal = async (req, res, next) => {
   const { type, breed, dob } = req.body;
@@ -18,10 +19,47 @@ exports.addAnimal = async (req, res, next) => {
   }
 };
 
+exports.getAnimal = async (req, res, next) => {
+  const animalId = req.body.animalId;
+  try {
+    const animal = await Animal.findById(animalId).populate(
+      "productionHistory"
+    );
+    res.status(200).json({ message: "Fetched animal", animal: animal });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
 exports.getAnimals = async (req, res, next) => {
   try {
     const animals = await Animal.find();
     res.status(200).json({ message: "Fetched animals", animals: animals });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
+exports.addProduct = async (req, res, next) => {
+  const { animalId, quantity } = req.body;
+  try {
+    const product = new Product({
+      quantity,
+      animal: animalId,
+    });
+    const result = await product.save();
+
+    const animal = await Animal.findById(animalId);
+    animal.productionHistory.push(result._id);
+    await animal.save();
+
+    res.status(201).json({ message: "Product added!" });
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
--- server/model/Animal.js
+++ server/model/Animal.js
@@ -1,9 +1,5 @@
 const mongoose = require("mongoose");
 
-const productSchema = require("./Product");
-const vaccinationSchema = require("./Vaccination");
-const breedingSchema = require("./Breeding");
-
 const Schema = mongoose.Schema;
 
 const animalSchema = new Schema({
@@ -23,9 +19,24 @@ const animalSchema = new Schema({
     type: String,
     default: "Healthy", // Healthy, Sick, Recovering
   },
-  productionHistory: [productSchema],
-  vaccinationHistory: [vaccinationSchema],
-  breedingHistory: [breedingSchema],
+  productionHistory: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+    },
+  ],
+  vaccinationHistory: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Vaccination",
+    },
+  ],
+  breedingHistory: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Breeding",
+    },
+  ],
 });
 
 module.exports = mongoose.model("Animal", animalSchema);
