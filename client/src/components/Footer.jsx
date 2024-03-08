e5baeecbe4a720c48eac1877e79bb29d66bd3d7f
Anushthan Saxena <anushthans@gmail.com>
2024-03-08 14:35:26 +0530

    landing and some other stuff

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,9 @@
 
 function Footer() {
   return (
-    <div className="w-full min-h-fit bg-color2 text-white font-bold p-2 footer-per" style={{position:"fixed", bottom:"0"}}>Footer</div>
+    <div className="w-full min-h-fit bg-color2 text-white font-bold p-2" style={{position:"fixed", bottom:"0"}}>
+      
+    </div>
   )
 }
 
