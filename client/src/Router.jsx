b1e78eb7feb75132c9197283c279c3a6c66234d7
Sosenk <shashankraj3636@gmail.com>
2024-03-08 12:32:51 +0530

    ola

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -7,11 +7,15 @@ import App from "./App";
 import {
     Landing
 } from './components'
+import { SignUp } from "./components/SignUp";
+import { Login } from "./components/Login";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<App />}>
         <Route index element={<Landing />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
         {/* <Route path="/rooms" element={<Rooms />} />
         <Route path="/book/:roomId" element={<BookForm />} />
         <Route path="/addRoom" element={<AddRoom />} />
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,7 @@
+
 function Footer() {
   return (
-    <div className="w-full min-h-fit bg-color2 text-white font-bold p-2">Footer</div>
+    <div className="w-full min-h-fit bg-color2 text-white font-bold p-2 footer-per" style={{position:"fixed", bottom:"0"}}>Footer</div>
   )
 }
 
