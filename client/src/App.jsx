52c0dc5abdbfadf6fb39616b3d11e4208138103d
Anushthan Saxena <anushthans@gmail.com>
2024-03-08 11:31:40 +0530

    routes added; providers fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
+import { Outlet } from "react-router-dom"
+import { Header, Footer } from "./components"
 const App = () => {
   return (
     <>
-     
+      <Header />
+      <Outlet />
+      <Footer />
     </>
   )
 }
 
-export default App
+export default App;
--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,1 +1,22 @@
-import { createBrowserRouter } from "react-router-dom";
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+  } from "react-router-dom";
+import App from "./App";
+import {
+    Landing
+} from './components'
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<App />}>
+        <Route index element={<Landing />} />
+        {/* <Route path="/rooms" element={<Rooms />} />
+        <Route path="/book/:roomId" element={<BookForm />} />
+        <Route path="/addRoom" element={<AddRoom />} />
+        <Route path="/checkout" element={<CheckoutForm />} /> */}
+      </Route>
+    )
+);
+export default router;
