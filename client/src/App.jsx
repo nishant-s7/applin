27414842c39e7b404e99d750fade61bd1d73af54
Anushthan Saxena <anushthans@gmail.com>
2024-03-08 12:37:52 +0530

    sticky style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,17 @@ import { Outlet } from "react-router-dom"
 import { Header, Footer } from "./components"
 const App = () => {
   return (
-    <>
-      <Header />
+    <section className="min-h-full w-full relative">
+      <div className="top-0 sticky w-full">
+        <Header />
+      </div>
+
       <Outlet />
-      <Footer />
-    </>
+
+      <div className="w-full bottom-0 sticky">
+        <Footer />
+      </div>
+    </section>
   )
 }
 
--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,6 +1,16 @@
+import { GiFarmTractor, GiMilkCarton } from "react-icons/gi";
 function Landing() {
   return (
-    <div>Landing</div>
+    <div className="max-w-full p-4 min-h-fit m-2 border border-red-600 rounded-lg flex flex-col justify-center items-center gap-8">
+
+        <div className="w-[175px] h-[175px] min-h-fit border-2 border-color2 rounded-2xl flex justify-center items-center">
+            <GiMilkCarton className="text-[150px]" />
+        </div>
+
+        <div className="w-[175px] h-[175px] min-h-fit border-2 border-color2 rounded-2xl flex justify-center items-center">
+            <GiFarmTractor className="text-[150px]" />
+        </div>
+    </div>
   )
 }
 
