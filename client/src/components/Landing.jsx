82483919b812cc16256235be4da76eea4b9ab34f
Anushthan Saxena <anushthans@gmail.com>
2024-03-08 16:04:58 +0530

    api changes and animal fetch

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,31 +1,38 @@
 import { GiCow, GiGoat, GiBuffaloHead } from "react-icons/gi";
 import Reveal from "./Reveal";
+import { Link } from "react-router-dom";
 
 function Landing() {
   return (
     <div className="flex flex-col justify-center items-center p-4 space-y-8">
 
       <Reveal>
-        <div className="flex flex-col items-center">
-          <GiCow className="text-[150px] text-color3 border-2 border-color1 rounded-2xl" />
-          <h2 className='text-3xl font-light text-color1'>Cow</h2>
-        </div>
+        <Link to={`/animal/Cow`}>
+          <div className="flex flex-col items-center">
+            <GiCow className="text-[150px] text-color3 border-2 border-color1 rounded-2xl" />
+            <h2 className='text-3xl font-light text-color1'>Cow</h2>
+          </div>
+        </Link>
       </Reveal>
 
 
       <Reveal>
-        <div className="flex flex-col items-center">
-          <GiGoat className="text-[150px] text-color3 border-2 border-color1 rounded-2xl" />
-          <h2 className='text-3xl font-light text-color1'>Goat</h2>
-        </div>
+        <Link to={`/animal/Goat`}>
+          <div className="flex flex-col items-center">
+            <GiGoat className="text-[150px] text-color3 border-2 border-color1 rounded-2xl" />
+            <h2 className='text-3xl font-light text-color1'>Goat</h2>
+          </div>
+        </Link>
       </Reveal>
 
 
       <Reveal>
-        <div className="flex flex-col items-center">
-          <GiBuffaloHead className="text-[150px] text-color3 border-2 border-color1 rounded-2xl" />
-          <h2 className='text-3xl font-light text-color1'>Buffalo</h2>
-        </div>
+        <Link to={`/animal/Buffalo`}>
+          <div className="flex flex-col items-center">
+            <GiBuffaloHead className="text-[150px] text-color3 border-2 border-color1 rounded-2xl" />
+            <h2 className='text-3xl font-light text-color1'>Buffalo</h2>
+          </div>
+        </Link>
       </Reveal>
     </div>
   )
